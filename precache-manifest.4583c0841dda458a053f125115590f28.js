ceec8cd5d54050cc10033ab3273c9d5bb265a06b
Shweta <shweta@antarx.tech>
2020-04-15 20:57:10 +0530

    Updates

---
+++ precache-manifest.4583c0841dda458a053f125115590f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bad627f421e66fd2af4949c65151f2f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "26f1cbf716f514ce1fe9",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0ee976d8c1fe593c3d6f",
+    "url": "/my-app/static/js/2.4ea1e38f.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/my-app/static/js/2.4ea1e38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f1cbf716f514ce1fe9",
+    "url": "/my-app/static/js/main.7fdcb831.chunk.js"
+  },
+  {
+    "revision": "1b58fcb5ce8d26145ee7",
+    "url": "/my-app/static/js/runtime-main.8b909c6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
